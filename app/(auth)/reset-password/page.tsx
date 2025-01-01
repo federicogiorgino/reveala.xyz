c2a8fde026c28d1c6738790375cd4de8acf7c44f
Federico Giorgino <59868572+federicogiorgino@users.noreply.github.com>
2025-01-01 02:14:08 +0100

    feat: wrapped reset-password-form with suspense

--- app/(auth)/reset-password/page.tsx
+++ app/(auth)/reset-password/page.tsx
@@ -1,7 +1,12 @@
 import { ResetPasswordForm } from "@/components/reset-password-form";
+import { Suspense } from "react";
 
 function ResetPasswordPage() {
-  return <ResetPasswordForm />;
+  return (
+    <Suspense>
+      <ResetPasswordForm />
+    </Suspense>
+  );
 }
 
 export default ResetPasswordPage;
